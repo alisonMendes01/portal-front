d701424b27d9fb7404c53a7e70739cd57c3c8ce4
Matheus Levy <levybessa2001@hotmail.com>
2022-07-29 17:20:38 -0300

    adicionado props

--- src/components/layout/EgressoLeft.js
+++ src/components/layout/EgressoLeft.js
@@ -2,21 +2,11 @@ import styles from './EgressoLeft.module.css';
 
 import foto from '../../img/foto.png';
 
-function EgressoLeft() {
+function EgressoLeft({texto}) {
     return (
         <p className={styles.paragrafo_left_right}>
             <img src={foto} alt="" className={styles.img_left} />
-            Fringilla nisl. Donec accumsan interdum nisi,quis tincidunt felis sagittis eget.
-            tempus euismod.Vestibulum ante ipsum primis in faucibus vestibulum.Blandit adipiscing
-            eu felis iaculis volutpat ac adipiscing accumInteger ac pellentesque praesent.san eu
-            faucibus.Integer ac pellentesque praesent tincidunt felis sagittis eget.tempus euismod.
-            Vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis
-            volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent.
-            Donec accumsan interdum nisi, quis tincidunt felis sagittis eget. tempus euismod.
-            Vestibulum ante ipsum primis in faucibus vestibulum. Blandit adipiscing eu felis iaculis
-            volutpat ac adipiscing accumsan eu faucibus. Integer ac pellentesque praesent tincidunt
-            felis sagittis eget.tempus euismod. Vestibulum ante ipsum primis in faucibus vestibulum.
-            Blandit adipiscing eu felis iaculis volutpat ac adipiscing accumsan eu faucibus.
+            <p>{texto}</p>
         </p>
     )
 }
