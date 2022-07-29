3edcbb285c4ed41a5eef48bb5660395a097ba57a
Matheus Levy <levybessa2001@hotmail.com>
2022-07-29 16:59:00 -0300

    const

--- src/components/pages/EgressoDescricao.js
+++ src/components/pages/EgressoDescricao.js
@@ -13,7 +13,6 @@ function EgressoDescricao() {
                 <EgressoContato nome={nome} formacao={formacao} ocupacao={ocupacao} anoConclusao= {anoConclusao}
                 UnidadeAcademica={UnidadeAcademica}/>
         </section>
-            
     );
 }
 
