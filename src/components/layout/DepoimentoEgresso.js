264d85b64867f618e7307626568356e3a17dbd0a
Matheus Levy <levybessa2001@hotmail.com>
2022-07-29 21:52:04 -0300

    merge depoimento

--- src/components/layout/DepoimentoEgresso.js
+++ src/components/layout/DepoimentoEgresso.js
@@ -3,7 +3,7 @@ import LinkImg from './LinkImg';
 import foto from '../../img/foto.png';
 
 
-function DepoimentoEgresso(link) {
+function DepoimentoEgresso({nome, titulo, texto, data}) {
     return (
         <div className={styles.container}>
             
@@ -12,11 +12,9 @@ function DepoimentoEgresso(link) {
                 <h2>{nome}</h2>
             </div>
 
-            <h1>What is Lorem Ipsum?</h1>
-            <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-            <h3>Data: XX/XX/XXXX</h3>
+            <h1>{titulo}</h1>
+            <p>{texto}</p>
+            <h3>Data: {data}</h3>
         </div>
     )
 }
--- src/components/pages/Depoimentos.js
+++ src/components/pages/Depoimentos.js
@@ -3,10 +3,13 @@ import styles from './Depoimento.module.css';
 
 function Depoimentos() {
     const nome = "Fulano de tal";
+    const titulo = "Titulo Teste";
+    const texto = "lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    const data = "2022";
     return (
         <div className={styles.home_depoimento}>
             <h1>Depoimentos</h1>
-            <DepoimentoEgresso />
+            <DepoimentoEgresso nome={nome} titulo={titulo} texto={texto} data={data}/>
         </div>
     );
 }
