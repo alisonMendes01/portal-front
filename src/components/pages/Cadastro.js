b187c78ddbf547e9962435fac6d5d6cd83e58c77
Kennedy Anderson <kennedy.anderson@discente.ufma.br>
2022-08-04 18:41:09 -0300

    atualização depoimento

--- src/components/pages/Cadastro.js
+++ src/components/pages/Cadastro.js
@@ -2,20 +2,9 @@ import style from './Cadastro.module.css';
 
 function Cadastro() {
     return (
-        <main className={style.container}>
-            <h2>Cadastro</h2>
-            <form method="post" action="">
-                <div  className={style.inputField}>
-                    <input type="text" name="email" id="email" placeholder="email" />
-                    <div className={style.underline}></div>
-                </div>
-                <div  className={style.inputField}>
-                    <input type="password" name="password" id="password" placeholder="senha"/>
-                    <div className={style.underline}></div>
-                </div>
-                <input type="submit" name="action" value="login" />
-            </form>
-        </main>
+        <div className={style.container}>
+            <h1>Cadastro</h1>
+        </div>
     );
 }
 
